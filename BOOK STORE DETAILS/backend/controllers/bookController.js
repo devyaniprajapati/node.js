95f7b9f0dc3e27408eda8336ae16276293fa2679
devyani prajapati <devyaniprajapati013@gmail.com>
2024-10-25 11:07:55 +0530

    update

--- BOOK STORE DETAILS/backend/controllers/bookController.js
+++ BOOK STORE DETAILS/backend/controllers/bookController.js
@@ -41,7 +41,7 @@ exports.deleteBook = async (req, res) => {
   try {
     const deletedBook = await Book.findByIdAndDelete(id);
     if (!deletedBook) return res.status(404).json({ error: "Book not found" });
-    res.status(200).json({ message: "Book deleted" });
+    res.status(200).json({ message: " deleted" });
   } catch (error) {
     res.status(500).json({ error: "Failed to delete book" });
   }
